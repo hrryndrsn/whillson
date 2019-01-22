fc2f1e68c1ba5495570ea99e586866765b6c9788
hrryndrsn <harryanderson9@gmail.com>
2019-01-22 20:52:05 +1100

    fixed app height and responsive

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,89 +1,144 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import {hillPathData, TapPathData} from './paths'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { hillPathData, TapPathData } from "./paths";
 
-import './App.css';
+import "./App.css";
 
 const ContainerWrapper = styled.div`
-  width: 100vw;
-  height: 90vh;
   margin: 0 auto;
-`
+  background: white;
+`;
 const SvgWrapper = styled.svg`
   width: 100%;
-  height: 1000px;
-`
+  background-color: #fff;
+  @media (max-width: 500px) {
+      margin-top: 20vh;
+  }
+`;
 const Hill = styled.path`
-  fill:#f2f2f2
-`
+  fill: #f2f2f2;
+`;
 const TapPath = styled.path`
-  stroke: rgba(0,0,0,0);
+  stroke: rgba(0, 0, 0, 0);
   stroke-width: 20;
   fill: none;
-  transition: stroke 200ms ease-out;
+  transition: stroke 200ms ease-out 50ms;
   &:hover {
-    stroke: #6FCF97;
+    stroke: #6fcf97;
     cursor: pointer;
   }
-`
+`;
 const MainPath = styled.path`
   stroke: none;
   fill: none;
-`
+`;
 
 class Container extends Component {
-
   state = {
     mouse: {
       x: 0,
-      y: 0,
+      y: 0
     },
-    tapPathActive: false
-  }
+    mousePoseOnLine: {
+      x: 0,
+      y: 0
+    },
+    mouseXPercent: 0,
+    tapPathActive: false,
+    points: [
+      {
+        x: 719.9999389648438,
+        y: 300,
+        color: "blue"
+      }
+    ]
+  };
 
   pathRef = React.createRef<SVGPathElement>();
 
   componentDidMount = () => {
-    window.addEventListener("load", (e) => {
-      const ref = this.pathRef.current
+    window.addEventListener("load", e => {
+      const ref = this.pathRef.current;
       if (!ref) {
-        console.log('ref is null')
+        console.log("ref is null");
       } else {
-        console.log(ref.getTotalLength())
+        this.getPointOnPath(ref, 0.5);
       }
     });
 
     window.addEventListener("mousemove", (e: MouseEvent) => {
-      this.setState({mouse: {x: e.x, y: e.y}})
+      // console.log(e.x/window.innerWidth)
+      // console.log(this.pathRef.current)
+      const xPct = e.x/window.innerWidth
+      const path = this.pathRef.current
+      let point;
+      if (path) {
+        let tryPoint = path.getPointAtLength(xPct)
+        if (tryPoint !== undefined) {
+          point = tryPoint
+        }
+        console.log(point)
+      }
+
+      let derp = point ? point : {x: 0, y: 0}
+      this.setState({ 
+        mouse: { x: e.x, y: e.y }, 
+        mouseXPercent: e.x / window.innerWidth,
+        mousePoseOnLine: {x: derp.x ? derp.x : 0, }
+      });
     });
-  }
+  };
+
+  getPointOnPath = (ref: SVGPathElement, pct: number) => {
+    const curveLen = ref.getTotalLength();
+    const point = ref.getPointAtLength(curveLen * pct);
+    return point;
+  };
 
   mouseOverTapPath = (e: React.MouseEvent) => {
-    this.setState({tapPathActive: true })
-  }
+    this.setState({ tapPathActive: true });
+  };
 
   mouseOutTapPath = (e: React.MouseEvent) => {
-    this.setState({tapPathActive: false })
-  }
-
+    this.setState({ tapPathActive: false });
+  };
 
   render() {
     return (
       <ContainerWrapper>
-        <SvgWrapper
-          viewBox="0 0 1440 900"
+        <SvgWrapper 
+                    width="100%"
+          viewBox={`0 0 1440 700`}
+          
         >
-        <Hill d={hillPathData} />
-        <TapPath d={TapPathData} 
-        onMouseOver={this.mouseOverTapPath}
-        onMouseOut={this.mouseOutTapPath}
-        
-        />
-        <MainPath d={TapPathData} ref={this.pathRef} />
-        </SvgWrapper>
+          <Hill d={hillPathData} />
+          <TapPath
+            d={TapPathData}
+            onMouseOver={this.mouseOverTapPath}
+            onMouseOut={this.mouseOutTapPath}
+          /> 
+          <MainPath d={TapPathData} ref={this.pathRef} />
+          <circle
+            cx={this.state.mousePoseOnLine.x}
+            cy={this.state.mousePoseOnLine.y}
+            r={25}
+            fill={"black"}
+          />
+        </SvgWrapper>        
       </ContainerWrapper>
     );
   }
 }
 
 export default Container;
+
+// {
+//   this.state.points.map( point => {
+//     <circle
+//     cx={point.x}
+//     cy={point.y}
+//     r={25}
+//     fill={"black"}
+//   />
+//   })
+// }
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -4,8 +4,12 @@ import styled from 'styled-components';
 import './App.css';
 
 const NavWrapper = styled.div`
-  height: 10vh;
+  height: 5vh;
   margin: 0 auto;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
 `
 
 class Nav extends Component {
